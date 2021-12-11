9b30de31039ddb1303f86706ea4ebf5695521e12
modern <Nasrin.gholami36300@gmail.com>
2021-12-11 18:46:48 +0330

    frist comit for Trello proj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './img/trello-logo.png';
+import trello from './img/trello.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="body">
+      <div className="header">
+        <div className="logo">
+          <a href="/home">
+            <img src={logo} alt="trello-logo"/>
+          </a>
+        </div>
+        <div className="sign-in-sign-up">
+          <div className="sign-in">
+            <a href="/login">
+              <span>Log in</span>
+            </a>
+          </div>
+          <div className="sign-up">
+            <a href="/sign up">
+              <spa>Sign up</spa>
+            </a>
+          </div>
+        </div>
+      </div>
+        <div className="content">
+        <div className="left-content">
+          <div className="main-content">
+             <h1>Trello helps teams move to work forward </h1>
+             <p>Collaborate, manage projects, and reach new productivity peaks.
+                   From high rises to the home office, 
+                   the way your team works is unique—accomplish it all with Trello.</p>
+          </div>
+          <form className="input-button">
+             <input name="email" type="email" placeholder="Email"/>
+             <div className="btn-control">
+                <button type="submit">Sign Up-it's free</button>
+              </div>
+                            
+          </form>
+          </div>
+        <div className="right-content">
+          <img  src={trello} style={{ width:450,height:450}} alt="Trello" />
+        </div>
+      </div>
+      </div>
+     
+    
   );
 }
 
